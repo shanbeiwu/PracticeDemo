6fb3d89288ee96ed10f7f2b07405b14b8be5ff2a
shanbeiwu <15129582545@163.com>
2020-03-05 19:13:27 +0800

    Delete user.sql

--- user.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `user` VALUES (1, '李四', 22, '男', NULL, '15129582545', '北京市海淀区');
-INSERT INTO `user` VALUES (2, '张三', 25, '男', '2020-3-5', '15129582546', '辽宁省大连市');
-INSERT INTO `user` VALUES (3, '王五', 22, '男', NULL, '15129582545', '北京市海淀区');
-INSERT INTO `user` VALUES (4, '赵柳', 25, '男', '2020-3-5', '15129582546', '辽宁省大连市');
